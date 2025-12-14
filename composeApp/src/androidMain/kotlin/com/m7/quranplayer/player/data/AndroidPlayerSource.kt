c09786fe9eb68924f23b2cbd42f13333b3bab3ff
mahmoudhawas <mahmoud.hawas@transflo.com>
2025-12-14 10:45:56 +0200

    - refactor: improve player controller and state handling - fix: reset player on release to avoid state issues

--- composeApp/src/androidMain/kotlin/com/m7/quranplayer/player/data/AndroidPlayerSource.kt
+++ composeApp/src/androidMain/kotlin/com/m7/quranplayer/player/data/AndroidPlayerSource.kt
@@ -10,18 +10,18 @@ import androidx.media3.session.SessionToken
 import com.google.common.util.concurrent.ListenableFuture
 import com.google.common.util.concurrent.MoreExecutors
 import com.m7.quranplayer.core.Log
-import com.m7.quranplayer.player.PlayerService
+import com.m7.quranplayer.player.PlayerSessionService
 import com.m7.quranplayer.player.domain.model.PlayerState
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.channels.Channel.Factory.CONFLATED
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.flow
 
-class AndroidPlayerSource(context: Context) : PlayerSource {
+class AndroidPlayerSource(private val context: Context) : PlayerSource {
 
     // handles service communication & session lifecycle
-    val controllerFuture: ListenableFuture<MediaController>
-    var player: MediaController? = null
+    private lateinit var controllerFuture: ListenableFuture<MediaController>
+    private var player: MediaController? = null
 
     override val playerState: Channel<PlayerState> = Channel(CONFLATED)
 
@@ -30,86 +30,99 @@ class AndroidPlayerSource(context: Context) : PlayerSource {
     private var playerError: Exception? = null
 
     init {
+        initController()
+    }
+
+    private fun initController() {
         val sessionToken = SessionToken(
             context,
-            ComponentName(context, PlayerService::class.java)
+            ComponentName(context, PlayerSessionService::class.java)
         )
         controllerFuture = MediaController.Builder(context, sessionToken).buildAsync()
         controllerFuture.addListener(
             {
                 player = controllerFuture.get()
-                    .also {
-                        it.prepare()
-                        it.addListener(object : Player.Listener {
-                            override fun onPlaybackStateChanged(playbackState: Int) {
-                                super.onPlaybackStateChanged(playbackState)
-                                Log("PlayerSource -> playbackState= $playbackState")
-                                playerError ?: run {
-                                    when (playbackState) {
-                                        Player.STATE_IDLE -> {
-                                            playerState.trySend(PlayerState.Idle)
-                                        }
-
-                                        Player.STATE_BUFFERING -> {
-                                            playerState.trySend(PlayerState.Loading)
-                                        }
-
-                                        Player.STATE_READY -> it.play()
-
-                                        Player.STATE_ENDED -> {
-                                            isCurrentItemEnded = true
-                                            playerState.trySend(PlayerState.Ended)
-                                        }
-                                    }
-                                }
-                            }
-
-                            override fun onIsPlayingChanged(isPlaying: Boolean) {
-                                super.onIsPlayingChanged(isPlaying)
-                                Log("PlayerSource -> isPlaying= $isPlaying")
-                                if (isPlaying) {
-                                    isCurrentItemEnded = false
-                                    playerState.trySend(
-                                        PlayerState.Playing(
-                                            duration = it.duration,
-                                            updatedPosition = flow {
-                                                it.apply {
-                                                    while (currentPosition <= duration) {
-                                                        delay(200)
-                                                        emit(currentPosition)
-                                                    }
-                                                }
-                                            }
-                                        ))
-                                } else if (!isCurrentItemEnded) {
-                                    playerState.trySend(PlayerState.Paused)
-                                }
-                            }
-
-                            override fun onPlayerErrorChanged(error: PlaybackException?) {
-                                super.onPlayerErrorChanged(error)
-                                Log("PlayerSource -> error= $error")
-                                playerError = error
-
-                                error?.also {
-                                    playerState.trySend(PlayerState.Error(error))
-                                }
-                            }
-                        })
-                    }
-
+                observePlayerUpdates()
             },
             MoreExecutors.directExecutor()
         )
     }
 
+    private fun getPlayer(): MediaController {
+        return player
+            ?: controllerFuture.get()
+                .also {
+                    player = it
+                }
+    }
+
+    private fun observePlayerUpdates() {
+        getPlayer().prepare()
+        getPlayer().addListener(object : Player.Listener {
+            override fun onPlaybackStateChanged(playbackState: Int) {
+                super.onPlaybackStateChanged(playbackState)
+                Log("PlayerSource -> playbackState= $playbackState")
+                playerError ?: run {
+                    when (playbackState) {
+                        Player.STATE_IDLE -> {
+                            playerState.trySend(PlayerState.Idle)
+                        }
+
+                        Player.STATE_BUFFERING -> {
+                            playerState.trySend(PlayerState.Loading)
+                        }
+
+                        Player.STATE_READY -> getPlayer().play()
+
+                        Player.STATE_ENDED -> {
+                            isCurrentItemEnded = true
+                            playerState.trySend(PlayerState.Ended)
+                        }
+                    }
+                }
+            }
+
+            override fun onIsPlayingChanged(isPlaying: Boolean) {
+                super.onIsPlayingChanged(isPlaying)
+                Log("PlayerSource -> isPlaying= $isPlaying")
+                if (isPlaying) {
+                    isCurrentItemEnded = false
+                    playerState.trySend(
+                        PlayerState.Playing(
+                            duration = getPlayer().duration,
+                            updatedPosition = flow {
+                                getPlayer().apply {
+                                    while (currentPosition <= duration) {
+                                        delay(200)
+                                        emit(currentPosition)
+                                    }
+                                }
+                            }
+                        ))
+                } else if (!isCurrentItemEnded) {
+                    playerState.trySend(PlayerState.Paused)
+                }
+            }
+
+            override fun onPlayerErrorChanged(error: PlaybackException?) {
+                super.onPlayerErrorChanged(error)
+                Log("PlayerSource -> error= $error")
+                playerError = error
+
+                error?.also {
+                    playerState.trySend(PlayerState.Error(error))
+                }
+            }
+        })
+    }
+
     override fun setItem(url: String) {
         currentItemUrl = url
-        player?.setMediaItem(MediaItem.fromUri(url))
+        getPlayer().setMediaItem(MediaItem.fromUri(url))
     }
 
     override fun play(url: String) {
-        player?.apply {
+        getPlayer().apply {
             playerError?.also {
                 // reset the player if there is an error to start over
                 prepare()
@@ -128,22 +141,24 @@ class AndroidPlayerSource(context: Context) : PlayerSource {
     }
 
     override fun pause() {
-        player?.pause()
+        getPlayer().pause()
     }
 
     override fun seekTo(positionMs: Long) {
-        player?.seekTo(positionMs)
+        getPlayer().seekTo(positionMs)
     }
 
     override fun repeat() {
-        player?.apply {
+        getPlayer().apply {
             seekTo(0)
             play()
         }
     }
 
     override fun release() {
-        player?.release()
+        getPlayer().release()
+        player = null
+        playerError = null
         currentItemUrl = null
     }
 }
